c56c3f6e98b64e71892caa3c369190ea111cd71c
Amanda Gxagxa <AmandaGxa@tfg.co.za>
2022-08-29 13:10:59 +0200

    changed color to #F7F7F7

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let names = document.getElementById('names')
+let subject = document.getElementById('subject')
+let email = document.getElementById('email')
+let message = document.getElementById('message')
+
