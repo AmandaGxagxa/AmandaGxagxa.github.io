5cf6b3b1cb8f6a7ce06b97824fd0c9884317baf5
Amanda Gxagxa <AmandaGxa@tfg.co.za>
2022-08-30 09:11:24 +0200

    fixed the skills section

--- js/index.js
+++ js/index.js
@@ -3,3 +3,22 @@ let subject = document.getElementById('subject')
 let email = document.getElementById('email')
 let message = document.getElementById('message')
 
+function validateForm() {
+    if (names.value == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+     if (subject.value == "") {
+      alert("Surname must be filled out");
+      return false;
+    }
+     if (email.value == "" || email.value ==) {
+      alert("number must be filled out");
+      return false;
+    }
+    if (message == "") {
+      alert("email must be filled out");
+      return false;
+    }
+  }
+   
