626fc453c437bccb12873b89a723bea160843996
Amanda Gxagxa <AmandaGxa@tfg.co.za>
2022-09-05 11:09:34 +0200

    finished my form validation

--- js/index.js
+++ js/index.js
@@ -1,36 +1,40 @@
-
-
-let subject = document.forms["myForm"]["subject"].value;
-let email = document.forms["myForm"]["email"].value
-let message = document.forms["myForm"]["message"].value;
 let nameErrorMsg = document.getElementById('name-error');
-let subErrorMsg = document.getElementById('email-error');
-let emailErrorMsg = document.getElementById('subject-error');
-// let btn_pink = document.getElementById('btn-pink')
+let emailErrorMsg = document.getElementById('email-error');
+let messageError = document.getElementById('message-error');
+let requiredFields = document.querySelectorAll(".requiredField")
+
 
 
 function validateForm() {
-    let names = document.forms["myForm"]["names"].value; 
-    if (names == "") {
-    nameErrorMsg.innerHTML = "Please enter a name"
+
+    const { names,email,message } = document.forms["myForm"]
+
+    if (names.value === "") {
+        nameErrorMsg.innerHTML = "Please enter a name"
         console.log("we are here")
-     return false;
-     
+        return false;
     }
-    if (email == "") {
-        nameErrorMsg.innerHTML = "Please enter a email address"
-            console.log("we are here")
-         return false;
-         
-        }
-       
-        if (subject == "") {
-            nameErrorMsg.innerHTML = "Please enter a subject"
-                console.log("we are here")
-             return false;
-             
-            }
-   
-  }
-   
-//btn_pink.addEventListener("click", validateForm);
+    if (email.value === "") {
+        emailErrorMsg.innerHTML = "Please enter a email address"
+        // console.log("we are here")
+        return false;
+
+    }
+    if(message.value === ""){
+        messageError.innerHTML = "Please enter a email address"
+
+        return false;
+    }
+
+
+}
+function clearErrorMsg(){
+    nameErrorMsg.innerHTML=""
+    messageError.innerHTML=""
+    emailErrorMsg.innerHTML=""
+}
+
+
+requiredFields.forEach(requiredField => {
+    requiredField.addEventListener('focus', clearErrorMsg)   
+});
